90477709e9bcd05fee82abd77e602afbd6429f3e
Raagasahithi Yarrabadi <sahithiyerrabadi@gmail.com>
2026-09-15 14:36:13 -0400

    Adjust padding and thickness for peptide rectangles

--- final_FLEXIFold.py
+++ final_FLEXIFold.py
@@ -5506,12 +5506,13 @@ with quant_diff_tab:
 
                             # Ensure very short peptides are visible as horizontal rectangles
                             length = r['end'] - r['start']
-                            pad = max(0.5, length * 0.07)
+                            pad = max(0.4, length * 0.05)
                             x_rect = [r['start'] - pad, r['end'] + pad, r['end'] + pad, r['start'] - pad, r['start'] - pad]
+                            thickness = 0.15
                             y_rect = [
-                                r['y_value'] - 0.06, r['y_value'] - 0.06,
-                                r['y_value'] + 0.06, r['y_value'] + 0.06,
-                                r['y_value'] - 0.06
+                                r['y_value'] - thickness, r['y_value'] - thickness,
+                                r['y_value'] + thickness, r['y_value'] + thickness,
+                                r['y_value'] - thickness
                             ]
 
                             hover_text = (
